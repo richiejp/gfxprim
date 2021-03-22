f09e499e7541197a111e35168d11a14c35d8e106
Richard Palethorpe <richiejp@f-m.fm>
2021-03-22 16:28:58 +0000

    add fill and resize

--- demos/automata/automata.c
+++ demos/automata/automata.c
@@ -13,7 +13,38 @@
 
 static void *uids;
 
-int pixmap_on_event
+static void fill_pixmap(gp_pixmap *p)
+{
+	gp_pixel col = gp_rgb_to_pixmap_pixel(0xff, 0xff, 0xff, p);
+
+	gp_fill(p, col);
+}
+
+static void allocate_backing_pixmap(gp_widget_event *ev)
+{
+	gp_widget *w = ev->self;
+
+	gp_pixmap_free(w->pixmap->pixmap);
+
+	w->pixmap->pixmap = gp_pixmap_alloc(w->w, w->h, ev->ctx->pixel_type);
+
+	fill_pixmap(w->pixmap->pixmap);
+}
+
+int pixmap_on_event(gp_widget_event *ev)
+{
+	gp_widget_event_dump(ev);
+
+	switch (ev->type) {
+	case GP_WIDGET_EVENT_RESIZE:
+		allocate_backing_pixmap(ev);
+	break;
+	default:
+	break;
+	}
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -25,7 +56,6 @@ int main(int argc, char *argv[])
 	gp_widget *pixmap = gp_widget_by_uid(uids, "pixmap", GP_WIDGET_PIXMAP);
 
 	gp_widget_event_unmask(pixmap, GP_WIDGET_EVENT_RESIZE);
-	gp_widget_event_unmask(pixmap, GP_WIDGET_EVENT_INPUT);
 
 	gp_widgets_main_loop(layout, "Pixmap example", NULL, argc, argv);
 
